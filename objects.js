46f15265a3cb81c15d54bab400326c3dc9682d00
jasjones89 <zebla9@gmail.com>
2017-10-14 19:32:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
 
-}
+  return playlist}
